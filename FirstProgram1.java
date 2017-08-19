fb49c3ddace9517381cd60f3d8c5b272554ad818
Tomasz G <supertomtomman@gmail.com>
2017-08-19 11:11:52 +0200

    first commit

---
+++ FirstProgram1.java
@@ -0,0 +1,12 @@
+/**
+ * Created by tganc on 19/08/2017.
+ */
+public class FirstProgram1 {
+
+    public static void main(String[] args) {
+        System.out.print("TEST");
+        System.out.print("\nHello World");
+
+    }
+
+}
